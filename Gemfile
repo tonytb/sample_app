c36d703393ef3dc17d3c6c66a95d83cdd0cb3d05
Tony <tstaunton@taxback.com>
2011-04-07 23:31:42 +0100

    Done with CRUD actions

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.4'
 gem 'gravatar_image_tag', '0.1.0'
+gem 'will_paginate', '~> 3.0.beta'
+
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -14,6 +16,7 @@ gem 'growl', '1.0.3'
 group :development do
 	gem 'rspec-rails', '2.5.0'
 	gem 'annotate-models', '1.0.4'
+	gem 'faker', '0.3.1'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
   
   def show
     @user = User.find(params[:id])
@@ -23,6 +30,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
   def edit
     @user = User.find(params[:id])
     @title = "Edit user"
@@ -43,6 +56,15 @@ class UsersController < ApplicationController
   
   def authenticate
     deny_access unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+  
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
   
 end
